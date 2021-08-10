7ed9aa3ed779c6f1f7a6cf13839a3e8aa76623bb
Ken Barrameda <87344336+captainleftovers@users.noreply.github.com>
2021-08-10 11:40:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
 const  quizData = [
     {
+      question: 'Who is the main character of One Piece?' ,
+      a: 'Zoro',
+      b:  'Bartolomeo',
+      c: ' Luffy',
+      d: 'Roger',
+
+      correct: 'c'
+    } , {
+        question: "Who was Luffy's first mate" ,
+        a: 'Nami',
+        b: 'Sanji',
+        c:'Chopper',
+        d:'Zoro',
+
+        correct: 'd'
+    }, {
+        question: 'Who is the Pirate King?',
+        a: 'Buggy',
+        b: 'Shanks',
+        c:'Roger',
+        d:'Ivankov',
         
+        correct: 'c'
+
+    } , {
+        question: 'What kind of animal is Chopper?',
+        a: 'Reindeer',
+        b:'Raccoon',
+        c:'Dog',
+        d:'Cat',
+
+        correct:'a'
+    } , {
+        question: "Who is Luffy's Father?",
+        a: 'Garp',
+        b:'Roger',
+        c:'Dragon',
+        d:'Mayor',
+
+        correct:'c'
+
     }
 ]
